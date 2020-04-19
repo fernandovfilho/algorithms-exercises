5bb698531d589a3555890bf8020ed119cf514c7e
Fernando Vargas Filho <fernandov.filho@gmail.com>
2020-04-19 19:08:43 -0300

    Quicksort order array

--- algorithms/array/quick-sort.js
+++ algorithms/array/quick-sort.js
@@ -1,7 +1,58 @@
-function quickSort(arr) {
-  return arr;
+"use strict";
+
+// Lucas Montano channel membership algorithm training
+
+/*
+
+Input Format
+
+The first argument is an unordered integer array
+The second argument is the first index to order array recursively
+The third argument is the pivot index to order array recursively
+
+Return an ordered integer array
+
+*/
+
+const { swap } = require("../../utils");
+
+let array = [];
+
+function quickSort(UnorderedArray, fromIndex, toIndex) {
+  if (array.length === 0) array = UnorderedArray;
+
+  let pivotIndex = toIndex;
+  let elementIndex = fromIndex;
+
+  while (elementIndex < pivotIndex) {
+    if (array[elementIndex] > array[pivotIndex]) {
+      array = swap(array, pivotIndex, pivotIndex - 1);
+
+      if (elementIndex + 1 < pivotIndex) {
+        array = swap(array, elementIndex, pivotIndex);
+      }
+
+      pivotIndex--;
+    } else {
+      elementIndex++;
+    }
+  }
+
+  let leftArrayFromIndex = 0;
+  let leftArrayToIndex = pivotIndex - 1;
+  let isLeftPivotArrayNotSorted = leftArrayToIndex > 1;
+  if (isLeftPivotArrayNotSorted) {
+    array = quickSort(array, leftArrayFromIndex, leftArrayToIndex);
+  }
+
+  let rightArrayFromIndex = pivotIndex + 1;
+  let rightArrayToIndex = array.length - 1;
+  let isRightPivotArrayNotSorted = rightArrayFromIndex < toIndex;
+  if (isRightPivotArrayNotSorted) {
+    array = quickSort(array, rightArrayFromIndex, rightArrayToIndex);
+  }
+
+  return array;
 }
 
-console.log("quickSort", quickSort([3, 7, 8, 5, 2, 1, 9, 5, 4]));
-
-//module.exports = quickSort;
+module.exports = quickSort;
--- tests/unit/array/quickSort.test.js
+++ tests/unit/array/quickSort.test.js
@@ -5,5 +5,7 @@ test("Order an array using quicksort", () => {
   const arrayToTest = [1, 10, 5, 8, 4, 6, 9, 7, 2, 3];
   const expectedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  expect(isSameArray(quickSort(arrayToTest), expectedArray)).toBe(true);
+  expect(
+    isSameArray(quickSort(arrayToTest, 0, arrayToTest.length), expectedArray)
+  ).toBe(true);
 });
